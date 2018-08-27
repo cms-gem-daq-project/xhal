bc7419032abdfcfe9794a04c0e97b4e4ef8c8f66
Brian Dorney <brian.l.dorney@cern.ch>
2018-08-27 16:24:44 +0200

    exposed all xhal classes to python library

--- xhalcore/src/common/python_wrappers/XHALInterface_python.cpp
+++ xhalcore/src/common/python_wrappers/XHALInterface_python.cpp
@@ -2,7 +2,12 @@
 #include "xhal/utils/Exception.h"
 #include "xhal/XHALDevice.h"
 #include "xhal/utils/PyTypes.h"
+#include "xhal/rpc/amc.h"
+#include "xhal/rpc/calibration_routines.h"
 #include "xhal/rpc/daq_monitor.h"
+#include "xhal/rpc/optohybrid.h"
+#include "xhal/rpc/utils.h"
+#include "xhal/rpc/vfat3.h"
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 #include <boost/python/suite/indexing/map_indexing_suite.hpp>
 
@@ -45,11 +50,16 @@ PY_EXCEPTION_TRANSLATOR(translate_XHALRPCNotConnectedException,xhal::utils::XHAL
 uint32_t (xhal::XHALDevice::*readReg_byname)(std::string regName) = &xhal::XHALDevice::readReg;
 uint32_t (xhal::XHALDevice::*readReg_byaddress)(uint32_t address) = &xhal::XHALDevice::readReg;
 
+//DaqMonitor
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getmonTRIGGERmain_overloads, getmonTRIGGERmain, 0, 1)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getmonTRIGGEROHmain_overloads, getmonTRIGGEROHmain, 0, 1)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getmonDAQOHmain_overloads, getmonDAQOHmain, 0, 1)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getmonOHmain_overloads, getmonOHmain, 0, 1)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getmonOHSCAmain_overloads, getmonOHSCAmain, 0, 2)
+
+//VFAT3
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(readVFAT3ADC_overloads, readVFAT3ADC, 2, 4)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(readVFAT3ADCMultiLink_overloads, readVFAT3ADCMultiLink, 3, 4)
 
 BOOST_PYTHON_MODULE(xhalpy){
   using namespace boost::python;
@@ -92,6 +102,20 @@ BOOST_PYTHON_MODULE(xhalpy){
   class_<NestedPyDict<int,PyDictUint32<std::string> > >("NestedPyDict")
     .def(map_indexing_suite<NestedPyDict<int,PyDictUint32<std::string> > >() );
 
+  class_<xhal::rpc::AMC>("AMC", init<const std::string&, const std::string&>())
+    .def("getOHVFATMask",&xhal::rpc::AMC::getOHVFATMask)
+    .def("getOHVFATMaskMultiLink",&xhal::rpc::AMC::getOHVFATMaskMultiLink)
+    .def("sbitReadOut",&xhal::rpc::AMC::sbitReadOut);
+
+  class_<xhal::rpc::CalRoutines>("CalRoutines", init<const std::string&>())
+    .def("checkSbitMappingWithCalPulse",&xhal::rpc::CalRoutines::checkSbitMappingWithCalPulse)
+    .def("checkSbitRateWithCalPulse",&xhal::rpc::CalRoutines::checkSbitRateWithCalPulse)
+    .def("genScan",&xhal::rpc::CalRoutines::genScan)
+    .def("genChannelScan",&xhal::rpc::CalRoutines::genChannelScan)
+    .def("sbitRateScan",&xhal::rpc::CalRoutines::sbitRateScan)
+    .def("ttcGenConf",&xhal::rpc::CalRoutines::ttcGenConf)
+    .def("ttcGenToggle",&xhal::rpc::CalRoutines::ttcGenToggle);
+
   class_<xhal::rpc::DaqMonitor>("DaqMonitor", init<const std::string&>())
     .def("getmonTTCmain",&xhal::rpc::DaqMonitor::getmonTTCmain)
     .def("getmonTRIGGERmain",&xhal::rpc::DaqMonitor::getmonTRIGGERmain,getmonTRIGGERmain_overloads())
@@ -100,6 +124,29 @@ BOOST_PYTHON_MODULE(xhalpy){
     .def("getmonOHmain",&xhal::rpc::DaqMonitor::getmonOHmain,getmonOHmain_overloads())
     .def("getmonDAQmain",&xhal::rpc::DaqMonitor::getmonDAQmain)
     .def("getmonOHSCAmain",&xhal::rpc::DaqMonitor::getmonOHSCAmain,getmonOHSCAmain_overloads());
+
+  class_<xhal::rpc::Optohybrid>("Optohybrid", init<const std::string&, const std::string&>())
+    .def("broadcastRead",&xhal::rpc::Optohybrid::broadcastRead)
+    .def("broadcastWrite",&xhal::rpc::Optohybrid::broadcastWrite)
+    .def("configureScanModule",&xhal::rpc::Optohybrid::configureScanModule)
+    .def("printScanConfiguration",&xhal::rpc::Optohybrid::printScanConfiguration)
+    .def("startScanModule",&xhal::rpc::Optohybrid::startScanModule)
+    .def("getUltraScanResults",&xhal::rpc::Optohybrid::getUltraScanResults)
+    .def("stopCalPulse2AllChannels",&xhal::rpc::Optohybrid::stopCalPulse2AllChannels);
+
+  class_<xhal::rpc::Utils>("Utils", init<const std::string&>())
+    .def("update_atdb",&xhal::rpc::Utils::update_atdb)
+    .def("getRegInfoDB",&xhal::rpc::Utils::getRegInfoDB);
+
+  class_<xhal::rpc::VFAT3>("VFAT3", init<const std::string&, const std::string&>())
+    .def("configureVFAT3s",&xhal::rpc::VFAT3::configureVFAT3s)
+    .def("configureVFAT3DacMonitor",&xhal::rpc::VFAT3::configureVFAT3DacMonitor)
+    .def("configureVFAT3DacMonitorMultiLink",&xhal::rpc::VFAT3::configureVFAT3DacMonitorMultiLink)
+    .def("getChannelRegistersVFAT3",&xhal::rpc::VFAT3::getChannelRegistersVFAT3)
+    .def("readVFAT3ADC",&xhal::rpc::VFAT3::readVFAT3ADC,readVFAT3ADC_overloads())
+    .def("readVFAT3ADCMultiLink",&xhal::rpc::VFAT3::readVFAT3ADCMultiLink,readVFAT3ADCMultiLink_overloads())
+    .def("setChannelRegistersVFAT3",&xhal::rpc::VFAT3::setChannelRegistersVFAT3)
+    .def("setChannelRegistersVFAT3Simple",&xhal::rpc::VFAT3::setChannelRegistersVFAT3Simple);
 }
 
 // Converts a C++ map to a python dict
