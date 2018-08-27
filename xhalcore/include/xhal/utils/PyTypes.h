7a6a50b90a692bbf92f6820abbcb4e4a803f86d6
Brian Dorney <brian.l.dorney@cern.ch>
2018-08-27 14:15:28 +0200

    added nested dictionary template type

--- xhalcore/include/xhal/utils/PyTypes.h
+++ xhalcore/include/xhal/utils/PyTypes.h
@@ -3,7 +3,15 @@
 #include <map>
 
 typedef std::vector<uint32_t> PyListUint32;
+
+//this is a typedef for std::map<T, uint32_t>
 template <class T>
 using PyDictUint32 = std::map<T,uint32_t>; //Now PyDictUint32 is a typedef for std::map<T, uint32_t>
+
+//this is a typedef for std::map<T, PyListUint32>
 template <class T>
-using PyDictVecUint32 = std::map<T, PyListUint32>; //now PyDictVecUint32 is a typedef for std::map<T, PyListUint32>
+using PyDictVecUint32 = std::map<T, PyListUint32>;
+
+//this is a typedef for std::map<T, M>
+template <class T, class M>
+using NestedPyDict std::map<T,M>
