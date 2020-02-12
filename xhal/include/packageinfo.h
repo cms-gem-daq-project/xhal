e25da5f18a83248aafeaf2ce26cd9a689008433c
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2020-02-12 16:10:39 +0100

    temporarily fixing dependencies

--- xhal/include/packageinfo.h
+++ xhal/include/packageinfo.h
@@ -3,15 +3,15 @@
 
 #ifndef DOXYGEN_IGNORE_THIS
 
-#define XHAL_REQUIRED_PACKAGE_LIST reedmuller,xerces-c
-#define XHAL_BASE_REQUIRED_PACKAGE_LIST reedmuller,xerces-c
-#define XHAL_CLIENT_REQUIRED_PACKAGE_LIST xhal-base,reedmuller,xerces-c
-#define XHAL_SERVER_REQUIRED_PACKAGE_LIST xhal-base,reedmuller,xerces-c,lmdb
+#define XHAL_REQUIRED_PACKAGE_LIST 
+#define XHAL_BASE_REQUIRED_PACKAGE_LIST 
+#define XHAL_CLIENT_REQUIRED_PACKAGE_LIST xhal
+#define XHAL_SERVER_REQUIRED_PACKAGE_LIST xhal
 
-#define XHAL_BUILD_REQUIRED_PACKAGE_LIST reedmuller-devel,gem-peta-stage-ctp7
-#define XHAL_BASE_BUILD_REQUIRED_PACKAGE_LIST reedmuller-devel,gem-peta-stage-ctp7
-#define XHAL_CLIENT_BUILD_REQUIRED_PACKAGE_LIST reedmuller-devel,gem-peta-stage-ctp7
-#define XHAL_SERVER_BUILD_REQUIRED_PACKAGE_LIST reedmuller-devel,lmdb-devel,gem-peta-stage-ctp7
+#define XHAL_BUILD_REQUIRED_PACKAGE_LIST gem-peta-stage-ctp7
+#define XHAL_BASE_BUILD_REQUIRED_PACKAGE_LIST gem-peta-stage-ctp7
+#define XHAL_CLIENT_BUILD_REQUIRED_PACKAGE_LIST gem-peta-stage-ctp7
+#define XHAL_SERVER_BUILD_REQUIRED_PACKAGE_LIST gem-peta-stage-ctp7
 
 #endif
 
